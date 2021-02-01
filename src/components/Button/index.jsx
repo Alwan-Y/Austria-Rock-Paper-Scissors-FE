81fcad027204852f22519a3ca48593ae8e09eb13
NicolaDonoastro <nicolaardiansya@gmail.com>
2021-02-01 21:33:31 +0700

    add Button & Modal components

---
+++ src/components/Button/index.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import classNames from "classnames";
+
+const Button = ({
+  label,
+  type,
+  size,
+  block,
+  onClick,
+  outline,
+  disabled,
+  className,
+  dataToggle,
+  dataTarget,
+  dataDismiss,
+}) => (
+  <button
+    type="button"
+    className={classNames(
+      "button btn",
+      { [`btn-outline-${type}`]: outline },
+      { [`btn-${type}`]: !outline },
+      { [`btn-${size}`]: size },
+      { "btn-block": block },
+      className
+    )}
+    disabled={disabled}
+    onClick={onClick}
+    data-toggle={dataToggle}
+    data-target={dataTarget}
+    data-dismiss={dataDismiss}
+  >
+    {label && label}
+  </button>
+);
+
+export default Button;
--- src/pages/JoinRoom/index.jsx
+++ src/pages/JoinRoom/index.jsx
@@ -1,6 +1,9 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
+import Modal from "components/Modal";
+import Button from "components/Button";
+
 import "./style.scss";
 
 const JoinRoom = () => {
@@ -15,87 +18,24 @@ const JoinRoom = () => {
 
   return (
     <>
-      <div className="container joinRoom">
-        <div className="row joinRoom__buttons ">
+      <div className="container join-room">
+        <div className="row join-room__button ">
           <div className="col-md-4">
-            <button
-              type="button"
-              className="btn btn-primary"
-              data-toggle="modal"
-              data-target="#exampleModalCenter"
-            >
-              Join Room
-            </button>
+            <Button
+              label="Join Room"
+              dataToggle="modal"
+              dataTarget="#exampleModalCenter"
+              type="primary"
+            />
           </div>
           <div className="col-md-4">
             <Link to="/game">
-              <div className="btn btn-secondary">Create Room</div>
+              <Button type="secondary" label="Create Room" />
             </Link>
           </div>
         </div>
       </div>
-
-      <div
-        className="modal fade"
-        id="exampleModalCenter"
-        tabIndex="-1"
-        role="dialog"
-        aria-labelledby="exampleModalCenterTitle"
-        aria-hidden="true"
-      >
-        <div className="modal-dialog modal-dialog-centered" role="document">
-          <div className="modal-content">
-            <div className="modal-header">
-              <h5 className="modal-title" id="exampleModalLongTitle">
-                Join Room
-              </h5>
-              <button
-                type="button"
-                className="close"
-                data-dismiss="modal"
-                aria-label="Close"
-              >
-                <span aria-hidden="true">&times;</span>
-              </button>
-            </div>
-            <div className="modal-body">
-              <form>
-                <div className="form-group">
-                  <label htmlFor="exampleInputEmail1">Room id</label>
-                  <input
-                    type="input"
-                    className="form-control"
-                    id="exampleInputEmail1"
-                    aria-describedby="emailHelp"
-                    placeholder="Enter room id"
-                    onChange={handleChange}
-                  />
-                  <small id="emailHelp" className="form-text text-muted">
-                    Input room id to join.
-                  </small>
-                </div>
-              </form>
-            </div>
-            <div className="modal-footer">
-              <button
-                type="button"
-                className="btn btn-secondary"
-                data-dismiss="modal"
-              >
-                Close
-              </button>
-              <button
-                type="button"
-                onClick={handleJoin}
-                className="btn btn-primary"
-                data-dismiss="modal"
-              >
-                Join Room
-              </button>
-            </div>
-          </div>
-        </div>
-      </div>
+      <Modal handleChange={handleChange} handleJoin={handleJoin} />
     </>
   );
 };
