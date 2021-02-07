737154613e7fdd70f7ad08a8f88fe29dae5df047
AlwanY <ebisukuru@gmail.com>
2021-02-07 16:53:55 +0700

    succes add design register-bio

---
+++ src/pages/RegisterBio/index.jsx
@@ -0,0 +1,71 @@
+import { useState } from "react";
+import { useHistory } from "react-router-dom";
+import fire from "../../configs/Firebase";
+import Button from "../../components/common/Button";
+import Form from "../../components/common/Form";
+import NavigationBar from "../../components/common/NavigationBar";
+import Footer from "../../components/common/Footer";
+
+const RegisterBio = () => {
+  const history = useHistory();
+  const [name, setName] = useState("");
+
+  const fetchUser = fire.auth().currentUser;
+  console.log(fetchUser);
+
+  const checkUser = async () => {
+    const user = await fire.auth().currentUser;
+
+    console.log(user.displayName);
+    const updateUser = await user.updateProfile({
+      displayName: name,
+    });
+    alert("Succes update display name");
+    console.log(user.displayName);
+  };
+
+  return (
+    <div>
+      <NavigationBar />
+      <div className="jumbotron jumbotron-fluid bio bio--background">
+        <div className="container">
+          <div className="row bio__possition">
+            <div className="col-lg"></div>
+            <div className="col-lg">
+              <Form
+                htmlFor="register-bio"
+                label="Your name?"
+                type="text"
+                id="register-bio"
+                placeholder="Enter your name here"
+                className="form-control bio__form"
+                classNameLabel="bio__label"
+                value={name}
+                onChange={(e) => {
+                  console.log(e.target.value);
+                  setName(e.target.value);
+                }}
+              />
+              <div className="row">
+                <div className="col-lg">
+                  <Button
+                    label="Register your name"
+                    type="button"
+                    block="true"
+                    type="primary"
+                    onClick={checkUser}
+                    className="bio__button"
+                  />
+                </div>
+              </div>
+            </div>
+            <div className="col"></div>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
+export default RegisterBio;
