724f70dcfa2ef527224048f217c85d1eec37e79f
AlwanY <ebisukuru@gmail.com>
2021-02-07 14:53:22 +0700

    secces create forgot password

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,7 @@ import Login from "./Login";
 import JoinRoom from "./JoinRoom";
 import Game from "./Game";
 import PageNotFound from "./PageNotFound";
-import ForgotPassword from "./ForgotPassword";
+import ForgotPassword from "./forgot-password";
 
 const Pages = {
   Home,
